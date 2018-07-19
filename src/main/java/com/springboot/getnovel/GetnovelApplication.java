6e17211391eae588205e5491798f4c894c13e537
zsz <864849506@qq.com>
2018-07-19 23:40:32 +0800

    搭建Springboot环境

---
+++ src/main/java/com/springboot/getnovel/GetnovelApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.getnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetnovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetnovelApplication.class, args);
+	}
+}
